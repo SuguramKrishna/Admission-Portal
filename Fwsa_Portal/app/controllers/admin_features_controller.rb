8bbba7af93a8645a952bcdec146c6142ca52ed55
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-08-22 09:25:20 +0530

    third push of sonarcloud bug fix

--- Fwsa_Portal/app/controllers/admin_features_controller.rb
+++ Fwsa_Portal/app/controllers/admin_features_controller.rb
@@ -96,15 +96,15 @@ class AdminFeaturesController < ApplicationController
     redirect_to '/admin_features/detailed_view'
   end
 
-  def edit
-    id = params[:id]
-    p id
-    @staff = Staff.find(id.to_i)
-    # $staff.each do |t|
-    #   p t.name
-    # end
-    render 'staff'
-  end
+  # def edit
+  #   id = params[:id]
+  #   p id
+  #   @staff = Staff.find(id.to_i)
+  #   # $staff.each do |t|
+  #   #   p t.name
+  #   # end
+  #   render 'staff'
+  # end
 
   private
 
